03854d0bbe783582cc650356444ee64ffbf49ab3
“[Krzysztof <[k.chylinski82@gmail.com]>
2022-05-08 00:09:14 +0100

    changed opacity of background image from 0.6 to 0.65

--- images.js
+++ images.js
@@ -4,34 +4,34 @@ let background = Math.floor(Math.random()*10);
 if(mediaQuery.matches) {
     switch(background) {
         case 0:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/portraits/p1.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/portraits/p1.jpg')";
             break;
         case 1:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/portraits/pexels-roberto-nickson-2486168.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/portraits/pexels-roberto-nickson-2486168.jpg')";
             break;
         case 2:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/portraits/p4.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/portraits/p4.jpg')";
             break;
         case 3:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/portraits/p5.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/portraits/p5.jpg')";
             break;
         case 4:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/portraits/p6.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/portraits/p6.jpg')";
             break;
         case 5:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/portraits/p7.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/portraits/p7.jpg')";
             break;
         case 6:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/portraits/p8.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/portraits/p8.jpg')";
             break;
         case 7:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/portraits/p9.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/portraits/p9.jpg')";
             break;
         case 8:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/portraits/p10.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/portraits/p10.jpg')";
             break;
         case 9:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/portraits/p11.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/portraits/p11.jpg')";
             break;
         default:
             window.alert('error');
@@ -40,34 +40,34 @@ if(mediaQuery.matches) {
 } else {
     switch(background) {
         case 0:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/landscapes/l01.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/landscapes/l01.jpg')";
             break;
         case 1:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/landscapes/pexels-roberto-nickson-landscape.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/landscapes/pexels-roberto-nickson-landscape.jpg')";
             break;
         case 2:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.6)), url('./resources/images/landscapes/l04.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/landscapes/l04.jpg')";
             break;
         case 3:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/landscapes/l05.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/landscapes/l05.jpg')";
             break;
         case 4:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/landscapes/l06.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/landscapes/l06.jpg')";
             break;
         case 5:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/landscapes/l07.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/landscapes/l07.jpg')";
             break;
         case 6:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/landscapes/l08.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/landscapes/l08.jpg')";
             break;
         case 7:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/landscapes/l09.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/landscapes/l09.jpg')";
             break;
         case 8:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/landscapes/l10.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/landscapes/l10.jpg')";
             break;
         case 9:
-            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.6), rgba(255,255,255,.6)), url('./resources/images/landscapes/l11.jpg')";
+            document.body.style.backgroundImage = "linear-gradient(rgba(255,255,255,.65), rgba(255,255,255,.65)), url('./resources/images/landscapes/l11.jpg')";
             break;
         default:
             window.alert('error');
